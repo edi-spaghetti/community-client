04d76826d2a9d7e5dbc999572ed9dab9cd987589
Ed Spencer <44098060+edi-spaghetti@users.noreply.github.com>
2023-02-11 20:43:53 +0000

    Move code and engine repo addresses

--- getDependencies.sh
+++ getDependencies.sh
@@ -43,6 +43,6 @@ cd ..
 
 ## Project Files
 
-git clone https://github.com/jasonrohrer/OneLife.git
-git clone https://github.com/jasonrohrer/minorGems.git
+git clone git@github.com:edi-spaghetti/community-code.git OneLife
+git clone git@github.com:edi-spaghetti/community-engine.git minorGems
 git clone https://github.com/jasonrohrer/OneLifeData7.git
